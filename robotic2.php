c89ccb634b135ad349688d5b80be4875a3c5f28e
blackranja <josphatranja243@gmail.com>
2021-04-25 20:19:42 +0300

    Adding more carousel objects

--- robotic2.php
+++ robotic2.php
@@ -3,8 +3,29 @@
 class RanjaRobot2{
     public function robotSlide1(){
         ?>
-        <div class="carousel"
-    }
+        <div class="carousel" slide carousel-fade" data-interval="20000" data-ride="carousel" id="robotCarousel">
+        <ol class="carousel-indicators">
+        <li data-target="#robotCarousel" data-slide-to="0" class="active"></li> 
+        <li data-target="#robotCarousel" data-slide-to="1" ></li> 
+        <li data-target="#robotCarousel" data-slide-to="2" ></li>
+        <li data-target="#robotCarousel" data-slide-to="3" ></li>
+        <li data-target="#robotCarousel" data-slide-to="4" ></li>
+        </ol>
+        <div class="carousel-inner">
+        <div class="active item">
+        <img src="images/img1" alt="PARTICAL IMAGE HERE" width="100%">
+        <div class="carousel-caption">
+        <h1><i class="text-primary">The Electron Particle</i>
+        <small>Electron with Electronics</small>
+        </h1>
+        </div>
+        </div>
+        <div class="item">
+        <img src="images/img2" alt="Another Electron Images Comes Here" width="100%">
+        <div class="carousel-caption">
+        <h1><i class="text-success">Ask For Developer</i><small>Have a developer develope for you...</small>
+        </div></div>
+        </div>
+        }
 }
-
 
