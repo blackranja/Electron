98abbd5a7d4e30f3fce2e13aab5ee12f30c8f04f
blackranja <josphatranja243@gmail.com>
2021-04-25 17:54:04 +0300

    The Second Object

---
+++ robotic2.php
@@ -0,0 +1,10 @@
+<?php 
+#The second files containing objects to be used for the robotics Projects
+class RanjaRobot2{
+    public function robotSlide1(){
+        ?>
+        <div class="carousel"
+    }
+}
+
+
